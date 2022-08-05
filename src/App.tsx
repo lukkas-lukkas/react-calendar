89164098ab6398ced732a328d842fa6bd9bea042
Lucas Lima <lukkasoliveiralima@gmail.com>
2022-08-05 18:33:32 -0300

    create style from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import style from './App.module.scss';
+import Card from './components/Card';
 
 export default function App() {
     return (
         <div className={style.app}>
-            Home app
+            <Card>
+                Teste
+            </Card>
         </div>
     );
 }
