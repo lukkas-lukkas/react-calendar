673972903d5c87d1b29d308cf032e2633ed4917e
Lucas Lima <lukkasoliveiralima@gmail.com>
2022-08-06 00:13:35 -0300

    create form to add new event

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import style from './App.module.scss';
 import Card from './components/Card';
+import Form from './components/Form';
 
 export default function App() {
     return (
         <div className={style.app}>
             <Card>
-                Teste
+                <Form />
             </Card>
         </div>
     );
