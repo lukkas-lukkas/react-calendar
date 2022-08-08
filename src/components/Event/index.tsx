23c8c99521e3feed482bbc1e6e13a13d11b4bf52
Lucas Lima <lukkasoliveiralima@gmail.com>
2022-08-08 19:04:36 -0300

    add button to delete

--- src/components/Event/index.tsx
+++ src/components/Event/index.tsx
@@ -2,11 +2,17 @@ import IEvent from 'interfaces/IEvent';
 import style from './Event.module.scss';
 
 export default function Event({ event }: { event: IEvent }) {
+    function removeEvent(event: IEvent) {
+        alert('Remove event => ' + event.description);
+    }
+
     return (
         <div className={style.event}>
             <div className="cards-info">
                 <h3 className={style.description}>{event.description} - {event.start.toLocaleDateString()}</h3>
             </div>
+            <i className="far fa-times-circle fa-2x" onClick={() => removeEvent(event)}></i>
         </div>
+        
     );
 }
