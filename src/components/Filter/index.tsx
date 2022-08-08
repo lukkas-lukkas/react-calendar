e49710812609bb8baba187576e07d85632c578f0
Lucas Lima <lukkasoliveiralima@gmail.com>
2022-08-08 17:40:48 -0300

    create form to filter

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,5 +1,32 @@
+import { useState } from 'react';
+import style from './Filter.module.scss';
+
 export default function Filter() {
+
+    const [date, setDate] = useState('');
+
+    function submeterForm(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        if (date) {
+            alert('DATE_TO_FILTER ' + date);
+        }
+    }
+
     return (
-        <div>Filter</div>
+        <form className={style.filter} onSubmit={submeterForm}>
+            <h3 className={style.title}>Filter by date</h3>
+            <input
+                type="date"
+                name="date"
+                className={style.input}
+                onChange={event => setDate(event.target.value)}
+                placeholder="By date"
+                value={date} />
+
+            <button className={style.button}>
+                Filter
+            </button>
+
+        </form>
     );
 }
