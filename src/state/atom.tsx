3e4d1f5bf0070d36c0178c3998fa0773c5edd134
Lucas Lima <lukkasoliveiralima@gmail.com>
2022-08-09 13:56:57 -0300

    retrivie events from api

--- src/state/atom.tsx
+++ src/state/atom.tsx
@@ -1,23 +1,11 @@
 import IEvent from 'interfaces/IEvent';
 import IFilter from 'interfaces/IFilter';
 import { atom } from 'recoil';
+import { apiEvents } from './selector';
 
 export const stateEventsList = atom<IEvent[]>({
     key: 'stateEventsList',
-    default: [
-        {
-            id: 1,
-            start: new Date('2022-08-08T05:00'),
-            end: new Date('2022-08-08T06:00'),
-            description: 'Summary state',
-        },
-        {
-            id: 2,
-            start: new Date('2022-08-09T08:00'),
-            end: new Date('2022-08-09T09:00'),
-            description: 'Summary state 2',
-        }
-    ] as IEvent[]
+    default: apiEvents,
 });
 
 export const stateFilter = atom<IFilter>({
--- src/state/selector.tsx
+++ src/state/selector.tsx
@@ -1,3 +1,5 @@
+import clientHttp from 'http/index';
+import IEvent from 'interfaces/IEvent';
 import { selector } from 'recoil';
 import { stateEventsList, stateFilter } from './atom';
 
@@ -15,4 +17,20 @@ export const filteredEvents = selector({
             return event.start.toISOString().slice(0, 10) === filter.date.toISOString().slice(0, 10);
         });
     },
+});
+
+export const apiEvents = selector({
+    key: 'apiEvents',
+    get: async () => {
+        const response = await clientHttp.get('/events');
+
+        const events: IEvent[] = response.data;
+
+        return events.map(event => {
+            event.start = new Date(event.start);
+            event.end = new Date(event.end);
+
+            return event;
+        });
+    }
 });
