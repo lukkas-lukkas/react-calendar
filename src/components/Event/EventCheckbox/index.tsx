cc1e58f6a51d0e882dacad6400a6d660a5d373b8
Lucas Lima <lukkasoliveiralima@gmail.com>
2022-08-09 11:15:31 -0300

    update event status

--- src/components/Event/EventCheckbox/index.tsx
+++ src/components/Event/EventCheckbox/index.tsx
@@ -1,16 +1,24 @@
+import { useUpdateEvent } from 'hooks/eventHooks';
 import IEvent from 'interfaces/IEvent';
 
 export default function EventCheckbox({ event }: { event: IEvent }) {
-
-    const isDone = false;
+    const updateEvent = useUpdateEvent();
 
     const styles = [
         'far',
         'fa-2x',
-        isDone ? 'fa-check-square' : 'fa-square',
+        event.done ? 'fa-check-square' : 'fa-square',
     ];
 
+    function changeStatus() {
+        const eventToUpdate = {...event};
+
+        eventToUpdate.done = !eventToUpdate.done;
+
+        updateEvent(eventToUpdate);
+    }
+
     return (
-        <i className={styles.join(' ')}></i>
+        <i className={styles.join(' ')} onClick={changeStatus}></i>
     );
 }
